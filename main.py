b7381e6f922a64c5a2c2452c2ddc2605bf73796a
Olanrewaju Toheeb Olawale <toheebolawale019@gmail.com>
2025-08-15 09:33:47 +0100

    Add initial OOP library system code with Book and Library classes

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
-    def get_title(self):
-        return self.title
+
 
 class Library:
     def __init__(self, name):
